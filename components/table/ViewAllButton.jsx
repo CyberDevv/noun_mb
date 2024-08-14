b84dda3cc281f3391ea1fc4fdf90e7a2ed88d7ee
Odesola Ibrahim <tocyberdevv@gmail.com>
2024-08-14 15:32:42 +0100

    ...

--- components/table/ViewAllButton.jsx
+++ components/table/ViewAllButton.jsx
@@ -6,7 +6,7 @@ const ViewAllButton = ({link}) => {
   return (
     <Button
       variant="link"
-      className="text-06 font-inter font-medium text-sm leading-[20px] space-x-1"
+      className="text-066 font-inter font-medium text-sm leading-[20px] space-x-1"
     >
       <span>View All</span>
       <Image src="/svg/ext.svg" width={16} height={16} alt="to page" />
