2a1bab0126124b28d31f272812149054baf75fe7
Odesola Ibrahim <tocyberdevv@gmail.com>
2024-08-28 17:53:55 +0100

    ...

--- components/RowFields.jsx
+++ components/RowFields.jsx
@@ -5,7 +5,7 @@ export const RowStatus = ({ label }) => (
   <div>
     <p className="font-medium text-00G capitalize leading-[20px] border rounded-full border-[#0FB48559] w-fit px-2 py-1 start space-x-1 mx-auto">
       <Image src="/svg/completed.svg" width={14} height={14} alt="completed" />
-      <span>{label.toLowerCase()}</span>
+      <span>{label?.toLowerCase()}</span>
     </p>
   </div>
 );
@@ -15,7 +15,11 @@ export const RowName = ({ label, naration }) => (
     <div className="h-[35px] w-[35px] rounded-full bg-gray-500" />
     <div>
       <p className="">{label}</p>
-      <p className={`${naration.toLowerCase() === "debit" ? "text-F7" : "text-00G"} capitalize text-[10px] leading-[14px] tracking-[0.3px] font-inter mt-0.5`}>
+      <p
+        className={`${
+          naration === "debit" ? "text-F7" : "text-00G"
+        } capitalize text-[10px] leading-[14px] tracking-[0.3px] font-inter mt-0.5`}
+      >
         {naration}
       </p>
     </div>
@@ -42,14 +46,24 @@ export const RowDate = ({ time, date }) => (
 
 export const RowStatusType2 = ({ label }) => (
   <div>
-    <p className={`${label === "ENABLED" ? "text-00G border-[#0FB48559]" : "text-[#D32F2F] border-[#D32F2F59]"} font-medium leading-[20px] border capitalize rounded-full w-fit px-2 py-1 start space-x-1 mx-auto`}>
+    <p
+      className={`${
+        label === "ENABLED"
+          ? "text-00G border-[#0FB48559]"
+          : "text-[#D32F2F] border-[#D32F2F59]"
+      } font-medium leading-[20px] border capitalize rounded-full w-fit px-2 py-1 start space-x-1 mx-auto`}
+    >
       <span>{label?.toLowerCase()}</span>
     </p>
   </div>
 );
 
 export const RowVerification = ({ label }) => (
-  <p className={`${label === "Approved" ? "text-00G bg-E6" : "bg-[#FEEDED] text-[#D32F2F]"}  leading-[20px] text-sm font-medium font-inter py-px px-2 rounded-full w-fit`}>
+  <p
+    className={`${
+      label === "Approved" ? "text-00G bg-E6" : "bg-[#FEEDED] text-[#D32F2F]"
+    }  leading-[20px] text-sm font-medium font-inter py-px px-2 rounded-full w-fit`}
+  >
     {label}
   </p>
 );
